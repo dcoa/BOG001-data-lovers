8bc28788d8204f6f1da15d3c2e66fb91da3958fd
dcoa <dcoa@live.com>
2020-07-01 11:31:36 -0500

    Código de Daniela en el main js, creación del index y characters.html

--- src/main.js
+++ src/main.js
@@ -1,21 +1,65 @@
 //import { example } from './data.js';
 // import data from './data/lol/lol.js';
 //import data from './data/pokemon/pokemon.js';
-import data from './data/rickandmorty/rickandmorty.js';
+//import data from './data/rickandmorty/rickandmorty.js';
 
-fetch('data/rickandmorty/rickandmorty.json')
-.then(response => response.json())
-.then(data =>{
-  console.log(data);
-});
+var personajesObj;
 
-fetch('https://rickandmortyapi.com/api/episode').then((response)=>{
-  return response.json();
-}).then((episode)=>{
-  console.log(episode);
-});
+const content = document.getElementById("content")
+const get = (url) => {
+
+   fetch(url)
+      .then(res => res.json())
+      .then(res => {
+         console.log(res);
+
+         personajesObj = res.info;
+         res.results.forEach(element => {
+            personajes.pintar(element);
+         });
+
+
+      });
+
+
+}
+const buttoncharacters = document.getElementById('button');
+buttoncharacters.addEventListener('click', get);
+
+const personajes = {
+   pintar: (personajesItem) => {
+
+      let divElement = document.createElement("div");
+
+      let imgElement = document.createElement("img");
+      imgElement.src = personajesItem.image;
+
+      let nameElement = document.createElement("h3");
+      nameElement.innerHTML = personajesItem.name;
+
+      let statusElement = document.createElement("p");
+      statusElement.innerHTML = personajesItem.status;
+
+      let speciesElement = document.createElement("p");
+      speciesElement.innerHTML = personajesItem.species;
+
+      divElement.appendChild(imgElement);
+      divElement.appendChild(nameElement);
+      divElement.appendChild(statusElement);
+      divElement.appendChild(speciesElement);
 
 
 
+      let optionsHtml = document.getElementById("container");
+      optionsHtml.appendChild(divElement);
 
-//  console.log(data);
+
+
+   },
+   more: () => {
+      if (personajesObj.next == "https://rickandmortyapi.com/api/character/?page=30") {
+         document.getElementById("seeMore").disabled = true;
+      }
+      get(personajesObj.next);
+   }
+};
