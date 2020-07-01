987871ff9e8ab8176cfeddab9724fd8ee498782e
dcoa <dcoa@live.com>
2020-06-30 19:49:48 -0500

    Usar fetch para traer los episodios y los datos

--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
-import { example } from './data.js';
+//import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+//import data from './data/pokemon/pokemon.js';
+import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+fetch('data/rickandmorty/rickandmorty.json')
+.then(response => response.json())
+.then(data =>{
+  console.log(data);
+});
+
+fetch('https://rickandmortyapi.com/api/episode').then((response)=>{
+  return response.json();
+}).then((episode)=>{
+  console.log(episode);
+});
+
+
+
+
+//  console.log(data);
