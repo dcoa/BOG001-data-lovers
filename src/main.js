c9065362494da452b064522c6eafbfa7241d730e
dcoa <dcoa@live.com>
2020-07-01 12:26:20 -0500

    prueba con js

--- src/main.js
+++ src/main.js
@@ -3,19 +3,22 @@
 //import data from './data/pokemon/pokemon.js';
 import data from './data/rickandmorty/rickandmorty.js';
 
+const container = document.getElementById('root');
+/*
 fetch('data/rickandmorty/rickandmorty.json')
 .then(response => response.json())
-.then(data =>{
-  console.log(data);
+.then(data => {
+    console.log(data.results);
 });
 
 fetch('https://rickandmortyapi.com/api/episode').then((response)=>{
   return response.json();
 }).then((episode)=>{
   console.log(episode);
-});
+});*/
 
 
 
 
-//  console.log(data);
+console.log(data);
+container.innerHTML = 'ggggg';
