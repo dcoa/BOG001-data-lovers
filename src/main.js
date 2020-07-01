9d571d1483750cfe96fee759816600d712dbc84e
dcoa <dcoa@live.com>
2020-07-01 12:46:50 -0500

    fetch

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 //import { example } from './data.js';
 // import data from './data/lol/lol.js';
 //import data from './data/pokemon/pokemon.js';
-import data from './data/rickandmorty/rickandmorty.js';
+//import data from './data/rickandmorty/rickandmorty.js';
 
 const container = document.getElementById('root');
-/*
+
 fetch('data/rickandmorty/rickandmorty.json')
 .then(response => response.json())
 .then(data => {
@@ -15,7 +15,7 @@ fetch('https://rickandmortyapi.com/api/episode').then((response)=>{
   return response.json();
 }).then((episode)=>{
   console.log(episode);
-});*/
+});
 
 
 
